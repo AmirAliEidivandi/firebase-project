d981ccfe25fe122ff5e1a4e441c8ac8410acbc48
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-07 00:04:36 +0430

    create upload image

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -15,8 +15,36 @@ const New = ({ inputs, title }) => {
     useEffect(() => {
         const uploadFile = () => {
             const name = new Date().getTime() + file.name;
+
             console.log(name);
-            // const storageRef = ref(storage, file.name)
+            const storageRef = ref(storage, file.name);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Upload is ", +Math.floor(progress) + "% done");
+                    switch (snapshot.state) {
+                        case "paused":
+                            console.log("Upload is paused");
+                            break;
+                        case "running":
+                            console.log("Upload is running");
+                            break;
+                        default:
+                            break;
+                    }
+                },
+                (error) => {
+                    console.log(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setData((prev) => ({ ...prev, img: downloadURL }));
+                    });
+                }
+            );
         };
         file && uploadFile();
     }, [file]);
